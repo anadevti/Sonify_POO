9da5d123832d81a01cf1602237f46eaef718d335
Ana Carolyne <anasouza.developer@gmail.com>
2024-11-08 12:03:59 -0300

    estruturando o projeto

---
+++ Sonify_POO/Music.cs
@@ -0,0 +1,24 @@
+﻿   using System.Collections.Concurrent;
+
+   namespace Sonify_POO;
+
+   class Music
+   {
+      public string Name { get; set; }
+      public string Artist { get; set; }
+      public int Duration { get; set; }
+      public bool Available { get; set; } // Leitura e escrita - Propriedade
+
+      public void Play()
+      {
+         Console.WriteLine($"Nome: {Name} - Artista: {Artist} - Duração: {Duration} - Disponível: {Available}");
+         if (Available)
+         {
+            Console.WriteLine("Disponivel para reprodução.");
+         }
+         else
+         {
+            Console.WriteLine("Música indisponível.");
+         }
+      }
+   }
--- Sonify_POO/Program.cs
+++ Sonify_POO/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Sonify_POO;
 
-Console.WriteLine("Hello, World!");
+Music music1 = new Music(); // instanciando um objeto da classe Music
+// atribuindo valores aos atributos do objeto music1:
+music1.Name = "Roxane";
+music1.Artist = "The Police";
+music1.Duration = 273;
+music1.Available = true;
+
+Music music2 = new Music(); // instanciando um objeto da classe Music
+music2.Name = "Bohemian Rhapsody";
+music2.Artist = "Queen";
+music2.Duration = 6;
+music2.Available = false;
+
+// chamando o método Play() para os objetos music1 e music2:
+music1.Play();
+music2.Play();
